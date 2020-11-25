3c4c3b53b571c394315578df0f929cbb93faffea
Bizet Rodriguez <brodriguez-velez@ucmerced.edu>
2020-11-24 19:40:12 -0800

    Changing frameworks main view from grid view to list view.

--- Apple-Frameworks/Screens/FrameworkDetailView/FrameworkDetailView.swift
+++ Apple-Frameworks/Screens/FrameworkDetailView/FrameworkDetailView.swift
@@ -19,8 +19,6 @@ struct FrameworkDetailView: View {
     
     var body: some View {
         VStack {
-            XDismissButton(isShowingDetailView: $isShowingDetailView)
-            
             Spacer()
             
             FrameworkTitleView(framework: framework)
